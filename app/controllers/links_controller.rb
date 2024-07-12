5c95a5eacf2acd6be320a7ca9c330b9111b37ddb
anouar elkhalfi <anouarelkhalfi@gmail.com>
2024-07-12 13:45:49 +0200

    link controller

---
+++ app/controllers/links_controller.rb
@@ -0,0 +1,15 @@
+class LinksController < ApplicationController
+  def index
+    @links = Link.all
+  end
+  def show
+    @link = Link.find(params[:id])
+  end
+  def new
+    @link = Link.new
+  end
+
+  def edit
+    @link = Link.find(params[:id])
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,23 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+puts "Cleaning database..."
+Link.destroy_all
+
+puts "Creating link records..."
+links = [
+  { title: "Rails Guides", url: "https://guides.rubyonrails.org/", description: "Official Ruby on Rails Guides" },
+  { title: "Stack Overflow", url: "https://stackoverflow.com/", description: "A community-driven question and answer website for programmers" },
+  { title: "GitHub", url: "https://github.com/", description: "A platform for version control and collaboration" },
+  { title: "DevDocs", url: "https://devdocs.io/", description: "A fast, offline documentation browser for developers" },
+  { title: "Ruby API", url: "https://ruby-doc.org/core-3.0.0/", description: "The Ruby core API documentation" },
+  { title: "CSS Tricks", url: "https://css-tricks.com/", description: "Tips, tricks, and techniques on using CSS" },
+  { title: "MDN Web Docs", url: "https://developer.mozilla.org/", description: "Resources for developers, by developers" },
+  { title: "FreeCodeCamp", url: "https://www.freecodecamp.org/", description: "Learn to code for free" },
+  { title: "Hacker News", url: "https://news.ycombinator.com/", description: "A social news website focusing on computer science and entrepreneurship" },
+  { title: "Smashing Magazine", url: "https://www.smashingmagazine.com/", description: "An online magazine for professional web designers and developers" }
+]
+
+links.each do |attributes|
+  link = Link.create!(attributes)
+  puts "Created #{link.title}"
+end
+
+puts "Finished!"
