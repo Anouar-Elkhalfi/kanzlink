ffa9f2892392a805efe7d8870502e445bc027a2a
anouar elkhalfi <anouarelkhalfi@gmail.com>
2024-07-19 14:02:00 +0200

    AddUser to link

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  # Devise routes for user authentication
+  devise_for :users
+  # Root route
+  root 'pages#home'
+  # Nested routes for collections and links
   resources :collections do
     resources :links, only: [:new, :create]
   end
+  # Routes for links outside the context of collections
   resources :links, except: [:new, :create]
-  devise_for :users
-   resources :links, except: [:new, :create]
-  root 'pages#home'
 end
